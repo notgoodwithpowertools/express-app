4c3144d1dc93b92694abbeac4af04814e01164b2
Andrew <notgoodwithpowertools@gmail.com>
2016-05-22 07:14:55 +1000

    Added GET response

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 var myPort = 8081;
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+//turn the server into an API server
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+var animals = [
+  "puppy",
+  "kitten",
+  "ferocious bear",
+  "penguin",
+  "tiger"
+]
+  
+app.get("/animals", function(req, res){
+  res.send(animals);
+});
 
 var dir =  process.cwd();
 console.log('PWDirectory...' + dir);
